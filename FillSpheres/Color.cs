5a395fd303eebff97ded2f60faec970d15d3def9
arbyun <someonealiveh@gmail.com>
2023-04-28 10:57:23 +0100

    Add methods for components RGBA and add GetGrey method

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -18,5 +18,15 @@
         public Color(int red, int green, int blue) : this(red, green, blue, 255)
         {
         }
+        
+        public int Red { get; set; }
+        
+        public int Green { get; set; }
+
+        public int Blue { get; set; }
+
+        public int Alpha { get; set; }
+
+        public int GetGrey() => _red + _green + _blue / 3;
     }
 }
