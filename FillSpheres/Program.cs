5e778b575acaf9a79c11cc8af6ace103f5099888
arbyun <someonealiveh@gmail.com>
2023-04-28 11:07:27 +0100

    Throw the spheres around

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -9,6 +9,13 @@ namespace FillSpheres
             Sphere sphere1 = new Sphere(new Color(255, 0, 0), 10.0, 0);
             Sphere sphere2 = new Sphere(new Color(0, 255, 0), 15.0, 0);
             Sphere sphere3 = new Sphere(new Color(0, 0, 255), 20.0, 0);
+            
+            for (int i = 0; i < 10; i++)
+            {
+                sphere1.Throw();
+                sphere2.Throw();
+                sphere3.Throw();
+            }
         }
     }
 }
