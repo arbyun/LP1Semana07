791ac2a7958a4a6694a980800036d2fb10faf09c
arbyun <someonealiveh@gmail.com>
2023-04-28 11:47:28 +0100

    Add string Name with Switch properties

--- BetterFillSpheres/Color.cs
+++ BetterFillSpheres/Color.cs
@@ -12,6 +12,24 @@ namespace BetterFillSpheres
         private int Blue { get; }
 
         private int Alpha { get; }
+        
+        public string Name
+        {
+            get
+            {
+                switch (ToString())
+                {
+                    case "#FF000000":
+                        return "Red 100%";
+                    case "#00FF00FF":
+                        return "Green 100%";
+                    case "#0000FFFF":
+                        return "Blue 100%";
+                    default:
+                        return "Mixed";
+                }
+            }
+        }
 
         public Color(int red, int green, int blue, int alpha)
         {
@@ -22,10 +40,17 @@ namespace BetterFillSpheres
         }
 
         private static int ValidateColorComponent(int value) => Math.Min(Math.Max(value, MinValue), MaxValue);
+        
+        public Color(int red, int green, int blue) : this(red, green, blue, 255) { }
 
         public int GetGrey()
         {
             return (Red + Green + Blue) / 3;
+        }
+        
+        public override string ToString()
+        {
+            return $"#{Red:X2}{Green:X2}{Blue:X2}{Alpha:X2}";
         }
     }
 }
