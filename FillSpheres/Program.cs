19a500a20fffaf727e53185817cfd0907471b213
arbyun <someonealiveh@gmail.com>
2023-04-28 11:19:43 +0100

    Change var name to reflect changes in Sphere.cs

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,9 +6,9 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Sphere sphere1 = new Sphere(new Color(255, 0, 0), 10.0, 0);
-            Sphere sphere2 = new Sphere(new Color(0, 255, 0), 15.0, 0);
-            Sphere sphere3 = new Sphere(new Color(0, 0, 255), 20.0, 0);
+            Sphere sphere1 = new Sphere(new Color(255, 1, 1), 10.0, 0);
+            Sphere sphere2 = new Sphere(new Color(1, 255, 1), 15.0, 0);
+            Sphere sphere3 = new Sphere(new Color(1, 1, 255), 20.0, 0);
             
             for (int i = 0; i < 10; i++)
             {
@@ -19,13 +19,13 @@ namespace FillSpheres
             
             sphere2.Pop();
 
-            Console.WriteLine($"Sphere 1 - Color: ({sphere1._color}), Radius: {sphere1._radius}, Times Thrown: " +
+            Console.WriteLine($"Sphere 1 - Color: ({sphere1.Color}), Radius: {sphere1.Radius}, Times Thrown: " +
                               $"{sphere1.GetTimesThrown()}");
 
-            Console.WriteLine($"Sphere 2 - Color: ({sphere2._color}), Radius: Popped, Times Thrown: " +
+            Console.WriteLine($"Sphere 2 - Color: ({sphere2.Color}), Radius: Popped, Times Thrown: " +
                               $"{sphere3.GetTimesThrown()}");
 
-            Console.WriteLine($"Sphere 3 - Color: ({sphere3._color}), Radius: {sphere3._radius}, Times Thrown: " +
+            Console.WriteLine($"Sphere 3 - Color: ({sphere3.Color}), Radius: {sphere3.Radius}, Times Thrown: " +
                               $"{sphere3.GetTimesThrown()}");
         }
     }
