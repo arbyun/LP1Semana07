32234c56ed90ed7c648f67b46531991298c95517
arbyun <someonealiveh@gmail.com>
2023-04-28 11:06:52 +0100

    Create spheres in main

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,7 +6,9 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Sphere sphere1 = new Sphere(new Color(255, 0, 0), 10.0, 0);
+            Sphere sphere2 = new Sphere(new Color(0, 255, 0), 15.0, 0);
+            Sphere sphere3 = new Sphere(new Color(0, 0, 255), 20.0, 0);
         }
     }
 }
