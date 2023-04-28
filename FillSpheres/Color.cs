2d4878a83348730d65f79825e3b5fbc7dc535084
arbyun <someonealiveh@gmail.com>
2023-04-28 10:56:43 +0100

    Initiate constructors

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -2,6 +2,21 @@
 {
     public class Color
     {
+        private int _red;
+        private int _green;
+        private int _blue;
+        private int _alpha;
         
+        public Color(int red, int green, int blue, int alpha)
+        {
+            _green = green;
+            _blue = blue;
+            _alpha = alpha;
+            _red = red;
+        }
+        
+        public Color(int red, int green, int blue) : this(red, green, blue, 255)
+        {
+        }
     }
 }
