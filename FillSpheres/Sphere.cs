616775fb45296088b9bb7fd2932220e968ff6ca3
arbyun <someonealiveh@gmail.com>
2023-04-28 11:02:25 +0100

    Add and initialize constructor

--- FillSpheres/Sphere.cs
+++ FillSpheres/Sphere.cs
@@ -2,6 +2,17 @@
 {
     public class Sphere
     {
+        private Color _color;
+        private double _radius;
+        private int _timesThrown;
+
+        public Sphere(Color color, double radius, int timesThrown)
+        {
+            _color = color;
+            _radius = radius;
+            _timesThrown = timesThrown;
+        }
+        
         
     }
 }
