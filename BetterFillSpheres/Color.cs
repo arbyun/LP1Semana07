131e3426df64d3bb6ec7d7bc320c087464709090
arbyun <someonealiveh@gmail.com>
2023-04-28 11:34:50 +0100

    Rewrite Color.cs using properties

---
+++ BetterFillSpheres/Color.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BetterFillSpheres
+{
+    public class Color
+    {
+        private const int MinValue = 0;
+        private const int MaxValue = 255;
+
+        private int Red { get; }
+        private int Green { get; }
+        private int Blue { get; }
+
+        private int Alpha { get; }
+
+        public Color(int red, int green, int blue, int alpha)
+        {
+            Red = ValidateColorComponent(red);
+            Green = ValidateColorComponent(green);
+            Blue = ValidateColorComponent(blue);
+            Alpha = ValidateColorComponent(alpha);
+        }
+
+        private static int ValidateColorComponent(int value) => Math.Min(Math.Max(value, MinValue), MaxValue);
+
+        public int GetGrey()
+        {
+            return (Red + Green + Blue) / 3;
+        }
+    }
+}
