626f8b715097b2c1858bc773399fea9184b70da9
arbyun <someonealiveh@gmail.com>
2023-04-28 12:02:57 +0100

    Implement class Player in a way that would be compatible with Program.cs

---
+++ PlayerStats/Player.cs
@@ -0,0 +1,40 @@
+﻿namespace PlayerStats
+{
+    public class Player
+    {
+        private float _highScore;
+        private int _playedGames;
+        private int _wonGames;
+
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public float HighScore
+        {
+            get => _highScore;
+            set
+            {
+                if (value > _highScore)
+                {
+                    _highScore = value;
+                }
+            }
+        }
+
+        public float WinRate => _playedGames == 0 ? 0 : (float)_wonGames / _playedGames;
+
+        public void PlayGame(bool win)
+        {
+            _playedGames++;
+
+            if (win)
+            {
+                _wonGames++;
+            }
+        }
+    }
+}
