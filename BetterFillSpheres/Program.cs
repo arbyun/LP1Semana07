5a773a9ed2756f8c28f96d4743852a423ae95275
arbyun <someonealiveh@gmail.com>
2023-04-28 11:47:56 +0100

    Add color.Name printing at the end of main

--- BetterFillSpheres/Program.cs
+++ BetterFillSpheres/Program.cs
@@ -6,8 +6,10 @@ namespace BetterFillSpheres
     {
         private static void Main(string[] args)
         {
-            Color red = new Color(255, 0, 0, 255);
+            Color red = new Color(255, 0, 0, 0);
             Color green = new Color(0, 255, 0, 255);
+            Color blue = new Color(0, 0, 255, 255);
+            Color mixed = new Color(128, 128, 128, 255);
             Sphere sphere1 = new Sphere(red, 10);
             Sphere sphere2 = new Sphere(green, 5);
 
@@ -20,7 +22,11 @@ namespace BetterFillSpheres
                               $"times thrown={sphere1.TimesThrown}");
             Console.WriteLine($"Sphere 2: color={sphere2.Color.GetGrey()}, radius={sphere2.Radius}, " +
                               $"times thrown={sphere2.TimesThrown}");
-
+            
+            Console.WriteLine(red.Name);
+            Console.WriteLine(green.Name);
+            Console.WriteLine(blue.Name);
+            Console.WriteLine(mixed.Name);
         }
     }
 }
