817b1849c231c345527955a1a689c83efb15f078
arbyun <someonealiveh@gmail.com>
2023-04-28 11:35:44 +0100

    Rewrote Sphere.cs using properties

--- BetterFillSpheres/Sphere.cs
+++ BetterFillSpheres/Sphere.cs
@@ -2,6 +2,30 @@
 {
     public class Sphere
     {
-        
+        private const int DefaultTimesThrown = 0;
+
+        private Color Color { get; }
+        private double Radius { get; set; }
+        private int TimesThrown { get; set; }
+
+        public Sphere(Color color, double radius)
+        {
+            Color = color;
+            Radius = radius;
+            TimesThrown = DefaultTimesThrown;
+        }
+
+        public void Pop()
+        {
+            Radius = 0;
+        }
+
+        public void Throw()
+        {
+            if (Radius > 0)
+            {
+                TimesThrown++;
+            }
+        }
     }
 }
